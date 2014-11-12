6c1372f29b33916f3e3272ccfdea9825e393f5e1
Yaadhav Raaj <raaj@nus.edu.sg>
2014-11-12 16:53:57 +0800

    a

--- Cruncher/locations.py
+++ Cruncher/locations.py
@@ -58,24 +58,185 @@ dicts=[
   },
   {
     "name": "SEMINAR\nTWO",
-    "actual": "Seminar Room Two",
+    "actual": "Seminar Room 2",
     "x": 33.30,
     "y": 9.34,
     "z": 0
   },
+  {
+	  "name": "P TEN",
+	  "actual": "P10",
+	  "x": 37.76,
+	  "y": 15.84,
+	  "z": 0
+  },
+  {
+    "name": "STUDENT\nAREA",
+    "actual": "Student Area",
+    "x": 56.03,
+    "y": 15.84,
+    "z": 0
+  },
+  {
+    "name": "SEMINAR\nONE",
+    "actual": "Seminar Room 1",
+    "x": 56.03,
+    "y": 29.24,
+    "z": 0
+  },
+  {
+    "name": "P ONE\nFOUR",
+    "actual": "P14",
+    "x": 70.65,
+    "y": 15.84,
+    "z": 0
+  },
+  {
+    "name": "P ONE\nFIVE",
+    "actual": "P15",
+    "x": 70.65,
+    "y": 28.02,
+    "z": 0
+  },
+  {
+    "name": "P ONE\nSIX",
+    "actual": "P16",
+    "x": 70.65,
+    "y": 7.31,
+    "z": 0
+  },
+  {
+    "name": "P ONE\nSEVEN",
+    "actual": "P17",
+    "x": 90.14,
+    "y": 28.02,
+    "z": 0
+  },
+  {
+    "name": "P ONE\nEIGHT",
+    "actual": "P18",
+    "x": 82.83,
+    "y": 7.31,
+    "z": 0
+  },
+  {
+    "name": "EXEC\nCLASS",
+    "actual": "Executive Classroom",
+    "x": 90.14,
+    "y": 21.93,
+    "z": 0
+  },
+  {
+    "name": "TUT ONE\nONE",
+    "actual": "Tutorial Room 11",
+    "x": 82.83,
+    "y": 10.56,
+    "z": 0
+  },
+  {
+    "name": "P TWO\nONE",
+    "actual": "P21",
+    "x": 94.60,
+    "y": 28.02,
+    "z": 0
+  },
+  {
+    "name": "P TWO\nTWO",
+    "actual": "P21",
+    "x": 97.44,
+    "y": 7.31,
+    "z": 0
+  },
+  {
+    "name": "SEMINAR\nNINE",
+    "actual": "Seminar Room 9",
+    "x": 94.60,
+    "y": 32.48,
+    "z": 0
+  },
+  {
+    "name": "P TWO\nFOUR",
+    "actual": "P24",
+    "x": 110.03,
+    "y": 28.02,
+    "z": 0
+  },
+  {
+    "name": "NUS\nHACKERS",
+    "actual": "NUS Hacker's Room",
+    "x": 97.44,
+    "y": 10.56,
+    "z": 0
+  },
+  {
+    "name": "P TWO\nSIX",
+    "actual": "P26",
+    "x": 110.03,
+    "y": 7.31,
+    "z": 0
+  },
+  {
+    "name": "SEMINAR\nELEVEN",
+    "actual": "Seminar Room 11",
+    "x": 110.03,
+    "y": 32.48,
+    "z": 0
+  },
+  {
+    "name": "P TWO\nEIGHT",
+    "actual": "P28",
+    "x": 110.03,
+    "y": 12.59,
+    "z": 0
+  },
+  {
+    "name": "P TWO\nNINE",
+    "actual": "P29",
+    "x": 115.71,
+    "y": 7.31,
+    "z": 0
+  },
+  {
+    "name": "CANTEEN",
+    "actual": "TO Canteen",
+    "x": 121.80,
+    "y": 7.31,
+    "z": 0
+  },
+  {
+    "name": "STAIRS\nONE ST",
+    "actual": "",
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },
+  {
+    "name": "STAIRS\nONE ED",
+    "actual": "",
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },  
+  {
+    "name": "CURB\nONE",
+    "actual": "",
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },
+  {
+    "name": "GLASS\nDOOR",
+    "actual": "",
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },
 
   {
-    "name": "CONTROL ROOM",
-    "actual": "CONTROL ROOM",
-    "x": 20,
-    "y": 20,
+    "name": "CONTROL\nROOM",
+    "actual": "",
+    "x": 0,
+    "y": 0,
     "z": 0
-  },
-  {
-    "name": "STAIRS",
-    "actual": "STAIRS",
-    "x": 20,
-    "y": 20,
-    "z": 0
-  },
+  }
 ]
