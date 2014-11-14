8b4829d97196b53a8b2a312e6bdbdf5a9f6477a5
Yaadhav Raaj <raaj@nus.edu.sg>
2014-11-15 07:49:21 +0800

    a

--- Cruncher/locations.py
+++ Cruncher/locations.py
@@ -4,266 +4,350 @@ dicts=[
     "name": "TO LT15",
     "actual": "TO LT15",
     "x": 0,
-    "y": 24.36,
+    "y": 53.22,
     "z": 0
   },
   {
     "name": "P TWO",
     "actual": "P2",
     "x": 21.52,
-    "y": 24.36,
+    "y": 53.22,
     "z": 0
   },
   {
     "name": "SEMINAR\nSIX",
     "actual": "Seminar Room 6",
     "x": 28.83,
-    "y": 29.24,
+    "y": 58.10,
     "z": 0
   },
   {
     "name": "LOBBY",
     "actual": "Lobby",
     "x": 37.76,
-    "y": 24.36,
+    "y": 53.22,
     "z": 0
   },
   {
     "name": "LINKWAY",
     "actual": "Linkway",
     "x": 21.52,
-    "y": 7.31,
+    "y": 36.17,
     "z": 0
   },
   {
     "name": "P FOUR",
     "actual": "P4",
     "x": 28.83,
-    "y": 24.36,
+    "y": 53.22,
     "z": 0
   },
   {
     "name": "P FIVE",
     "actual": "P5",
     "x": 28.83,
-    "y": 15.84,
+    "y": 46.73,
     "z": 0
   },
   {
     "name": "P EIGHT",
     "actual": "P8",
     "x": 33.30,
-    "y": 15.84,
+    "y": 46.73,
     "z": 0
   },
   {
     "name": "SEMINAR\nTWO",
     "actual": "Seminar Room 2",
     "x": 33.30,
-    "y": 9.34,
+    "y": 38.20,
     "z": 0
   },
   {
   "name": "P TEN",
   "actual": "P10",
   "x": 37.76,
-  "y": 15.84,
+  "y": 46.73,
   "z": 0
   },
   {
     "name": "STUDENT\nAREA",
     "actual": "Student Area",
     "x": 56.03,
-    "y": 15.84,
+    "y": 46.73,
     "z": 0
   },
   {
     "name": "SEMINAR\nONE",
     "actual": "Seminar Room 1",
     "x": 56.03,
-    "y": 29.24,
+    "y": 58.10,
+    "z": 0
+  },
+  {
+    "name": "P ONE\nTHREE",
+    "actual": "P13",
+    "x": 56.03,
+    "y": 34.95,
     "z": 0
   },
   {
     "name": "P ONE\nFOUR",
     "actual": "P14",
     "x": 70.65,
-    "y": 15.84,
+    "y": 46.73,
     "z": 0
   },
   {
     "name": "P ONE\nFIVE",
     "actual": "P15",
     "x": 70.65,
-    "y": 28.02,
+    "y": 56.88,
     "z": 0
   },
   {
     "name": "P ONE\nSIX",
     "actual": "P16",
     "x": 70.65,
-    "y": 7.31,
+    "y": 36.17,
     "z": 0
   },
   {
     "name": "P ONE\nSEVEN",
     "actual": "P17",
     "x": 90.14,
-    "y": 28.02,
+    "y": 56.88,
     "z": 0
   },
   {
     "name": "P ONE\nEIGHT",
     "actual": "P18",
     "x": 82.83,
-    "y": 7.31,
+    "y": 36.17,
     "z": 0
   },
   {
     "name": "EXEC\nCLASS",
     "actual": "Executive Classroom",
     "x": 90.14,
-    "y": 21.93,
+    "y": 50.79,
     "z": 0
   },
   {
     "name": "TUT ONE\nONE",
     "actual": "Tutorial Room 11",
     "x": 82.83,
-    "y": 10.56,
+    "y": 39.42,
     "z": 0
   },
   {
     "name": "P TWO\nONE",
     "actual": "P21",
     "x": 94.60,
-    "y": 28.02,
+    "y": 56.88,
     "z": 0
   },
   {
     "name": "P TWO\nTWO",
-    "actual": "P21",
+    "actual": "P22",
     "x": 97.44,
-    "y": 7.31,
+    "y": 36.17,
     "z": 0
   },
   {
     "name": "SEMINAR\nNINE",
     "actual": "Seminar Room 9",
     "x": 94.60,
-    "y": 32.48,
+    "y": 61.34,
     "z": 0
   },
   {
     "name": "P TWO\nFOUR",
     "actual": "P24",
     "x": 110.03,
-    "y": 28.02,
+    "y": 56.88,
     "z": 0
   },
   {
     "name": "NUS\nHACKERS",
     "actual": "NUS Hacker's Room",
     "x": 97.44,
-    "y": 10.56,
+    "y": 39.42,
     "z": 0
   },
   {
     "name": "P TWO\nSIX",
     "actual": "P26",
     "x": 110.03,
-    "y": 7.31,
+    "y": 35.77,
     "z": 0
   },
   {
     "name": "SEMINAR\nELEVEN",
     "actual": "Seminar Room 11",
     "x": 110.03,
-    "y": 32.48,
+    "y": 61.34,
     "z": 0
   },
   {
     "name": "P TWO\nEIGHT",
     "actual": "P28",
     "x": 110.03,
-    "y": 12.59,
+    "y": 41.45,
     "z": 0
   },
   {
     "name": "P TWO\nNINE",
     "actual": "P29",
-    "x": 115.71,
-    "y": 7.31,
+    "x": 119.21,
+    "y": 35.77,
     "z": 0
   },
   {
     "name": "CANTEEN",
     "actual": "TO Canteen",
     "x": 121.80,
-    "y": 7.31,
+    "y": 36.17,
+    "z": 0
+  },
+  {
+    "name": "TO COM2-2-1",
+    "actual": "TO COM2-2-1",
+    "x": 118.15,
+    "y": 32.92,
+    "z": 0
+  },
+  {
+    "name": "P THREE\nTWO",
+    "actual": "P32",
+    "x": 75.52,
+    "y": 56.88,
+    "z": 0
+  },
+  {
+    "name": "SEMINAR\nSEVEN",
+    "actual": "Seminar Room 7",
+    "x": 75.52,
+    "y": 59.72,
+    "z": 0
+  },
+  {
+    "name": "P THREE\nFOUR",
+    "actual": "P34",
+    "x": 102.72,
+    "y": 36.17,
+    "z": 0
+  },
+  {
+    "name": "TUT FIVE",
+    "actual": "Tutorial Room 5",
+    "x": 102.72,
+    "y": 33.33,
+    "z": 0
+  },
+  {
+    "name": "CEREBRO",
+    "actual": "Cerebro",
+    "x": 42.63,
+    "y": 34.95,
+    "z": 0
+  },
+  {
+    "name": "P THREE\nSEVEN",
+    "actual": "P37",
+    "x": 70.65,
+    "y": 44.29,
+    "z": 0
+  },
+  {
+    "name": "SR3 FRONT",
+    "actual": "SR3 front",
+    "x": 75.52,
+    "y": 44.29,
+    "z": 0
+  },
+  {
+    "name": "P THREE\nNINE",
+    "actual": "P39",
+    "x": 88.11,
+    "y": 56.88,
+    "z": 0
+  },
+  {
+    "name": "SR3 BACK",
+    "actual": "SR3 Back",
+    "x": 88.11,
+    "y": 53.22,
+    "z": 0
+  },
+  {
+    "name": "TO COM1-2-31",
+    "actual": "TO COM1-2-31",
+    "x": 118.15,
+    "y": 32.92,
     "z": 0
   },
   {
     "name": "STAIRS\nONE ST",
-    "actual": "",
+    "actual": "Stairs 1 Start",
     "x": 21.52,
-    "y": 24.36,
+    "y": 53.22,
     "z": 0
   },
   {
     "name": "STAIRS\nONE ED",
-    "actual": "",
+    "actual": "Stairs 1 End",
     "x": 21.52,
-    "y": 7.31,
+    "y": 36.17,
     "z": 0
   },  
   {
     "name": "STAIRS\nTWO ST",
-    "actual": "",
+    "actual": "Stairs 2 Start",
     "x": 110.03,
-    "y": 12.59,
+    "y": 41.45,
     "z": 0
   },
   {
     "name": "STAIRS\nTWO ED",
-    "actual": "",
+    "actual": "Stairs 2 End",
     "x": 121.80,
-    "y": 7.31,
+    "y": 36.17,
     "z": 0
   }, 
   {
     "name": "CURB\nONE",
-    "actual": "",
+    "actual": "Curb 1",
     "x": 56.03,
-    "y": 11.84,
+    "y": 43.23,
     "z": 0
   },
   {
     "name": "CURB\nTWO",
-    "actual": "",
+    "actual": "Curb 2",
     "x": 70.65,
-    "y": 11.84,
+    "y": 43.23,
     "z": 0
   },
   {
     "name": "GLASS\nONE",
-    "actual": "",
+    "actual": "Glass 1",
     "x": 42.76,
-    "y": 15.84,
+    "y": 46.73,
     "z": 0
   },
   {
     "name": "GLASS\nTWO",
-    "actual": "",
+    "actual": "Glass 2",
     "x": 71.65,
-    "y": 28.02,
+    "y": 56.88,
     "z": 0
   },
   {
     "name": "GLASS\nTHREE",
-    "actual": "",
+    "actual": "Glass 3",
     "x": 109.03,
-    "y": 7.31,
+    "y": 41.45,
     "z": 0
   }
 ]
